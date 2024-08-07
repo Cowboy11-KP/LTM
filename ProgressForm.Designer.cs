49f15d971d27e167f7e7162a586feb59a006af7d
Trần Ngọc Nhẫn <152972587+Cowboy11-KP@users.noreply.github.com>
2024-08-08 03:30:16 +0700

    Tạo một winForm để hiện thị thanh loadding...

---
+++ ProgressForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace UploadFileGoogleDrive
+{
+    partial class ProgressForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Clean up any resources being used.
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(16, 15);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(347, 28);
+            this.progressBar.TabIndex = 0;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(16, 47);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(79, 16);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Uploading...";
+            // 
+            // ProgressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 75);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.progressBar);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ProgressForm";
+            this.Text = "ProgressForm";
+            this.Load += new System.EventHandler(this.ProgressForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
---
+++ ProgressForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UploadFileGoogleDrive
+{
+    public partial class ProgressForm : Form
+    {
+        public ProgressForm()
+        {
+            InitializeComponent();
+        }
+
+        // Expose the ProgressBar and StatusLabel to be accessible from outside
+        public ProgressBar ProgressBar => this.progressBar;
+        public Label StatusLabel => this.statusLabel;
+
+        private void ProgressForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
