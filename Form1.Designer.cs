450892b89028861a41370cfbfc077c1dc2af13c3
khuong1201 <147017053+khuong1201@users.noreply.github.com>
2024-08-14 08:47:00 +0700

    up

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Windows.Forms;
-namespace UploadGoogleDrive
+﻿namespace ProgressForm
 {
     partial class Form1
     {
@@ -21,170 +19,55 @@ namespace UploadGoogleDrive
             }
             base.Dispose(disposing);
         }
+
+        #region Windows Form Designer generated code
+
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-
-            this.label2 = new System.Windows.Forms.Label();
-            this.buttonSelectFile = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.buttonUpdate = new System.Windows.Forms.Button();
-            this.listBoxFiles = new System.Windows.Forms.ListBox();
-            this.buttonRemove = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.labelNameAddress = new System.Windows.Forms.Label();
-            this.buttonChangeAccount = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.statusLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label2
+            // progressBar
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(29, 29);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 25);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "FILE:";
-            this.label2.Click += new System.EventHandler(this.labelTextFile_Click);
+            this.progressBar.Location = new System.Drawing.Point(16, 15);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(347, 28);
+            this.progressBar.TabIndex = 0;
             // 
-            // buttonSelectFile
+            // statusLabel
             // 
-            this.buttonSelectFile.Location = new System.Drawing.Point(99, 22);
-            this.buttonSelectFile.Name = "buttonSelectFile";
-            this.buttonSelectFile.Size = new System.Drawing.Size(78, 32);
-            this.buttonSelectFile.TabIndex = 3;
-            this.buttonSelectFile.Text = "Select";
-            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(16, 47);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(79, 16);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Uploading...";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(29, 108);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(194, 25);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "LIST UPLOAD FILE:";
-            this.label3.Click += new System.EventHandler(this.labelTextLUd_Click);
-            // 
-            // buttonUpdate
-            // 
-            this.buttonUpdate.Location = new System.Drawing.Point(603, 147);
-            this.buttonUpdate.Name = "buttonUpdate";
-            this.buttonUpdate.Size = new System.Drawing.Size(78, 30);
-            this.buttonUpdate.TabIndex = 6;
-            this.buttonUpdate.Text = "Upload";
-            this.buttonUpdate.UseVisualStyleBackColor = true;
-            // 
-            // listBoxFiles
-            // 
-            this.listBoxFiles.AllowDrop = true;
-            this.listBoxFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBoxFiles.FormattingEnabled = true;
-            this.listBoxFiles.ItemHeight = 20;
-            this.listBoxFiles.Location = new System.Drawing.Point(34, 147);
-            this.listBoxFiles.Name = "listBoxFiles";
-            this.listBoxFiles.Size = new System.Drawing.Size(545, 244);
-            this.listBoxFiles.TabIndex = 5;
-            this.listBoxFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBoxFiles_DragDrop);
-            this.listBoxFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBoxFiles_DragEnter);
-            // 
-            // buttonRemove
-            // 
-            this.buttonRemove.Location = new System.Drawing.Point(605, 213);
-            this.buttonRemove.Name = "buttonRemove";
-            this.buttonRemove.Size = new System.Drawing.Size(76, 30);
-            this.buttonRemove.TabIndex = 7;
-            this.buttonRemove.Text = "Remove";
-            this.buttonRemove.UseVisualStyleBackColor = true;
-            this.buttonRemove.Click += new System.EventHandler(this.ButtonRemove_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(259, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 25);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Account: ";
-            this.label1.Click += new System.EventHandler(this.labelTextAcc_Click);
-            // 
-            // labelNameAddress
-            // 
-            this.labelNameAddress.AutoSize = true;
-            this.labelNameAddress.Location = new System.Drawing.Point(372, 37);
-            this.labelNameAddress.Name = "labelNameAddress";
-            this.labelNameAddress.Size = new System.Drawing.Size(97, 16);
-            this.labelNameAddress.TabIndex = 9;
-            this.labelNameAddress.Text = "Name address";
-            this.labelNameAddress.Click += new System.EventHandler(this.labelNameAddress_Click);
-            // 
-            // buttonChangeAccount
-            // 
-            this.buttonChangeAccount.Location = new System.Drawing.Point(576, 22);
-            this.buttonChangeAccount.Name = "buttonChangeAccount";
-            this.buttonChangeAccount.Size = new System.Drawing.Size(131, 36);
-            this.buttonChangeAccount.TabIndex = 10;
-            this.buttonChangeAccount.Text = "Change Account";
-            this.buttonChangeAccount.UseVisualStyleBackColor = true;
-            this.buttonChangeAccount.Click += new System.EventHandler(this.ButtonChangeAccount_Click);
-            // 
-            // Form1
+            // ProgressForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(735, 450);
-            this.Controls.Add(this.buttonChangeAccount);
-            this.Controls.Add(this.labelNameAddress);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.buttonRemove);
-            this.Controls.Add(this.buttonUpdate);
-            this.Controls.Add(this.listBoxFiles);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.buttonSelectFile);
-            this.Controls.Add(this.label2);
-            this.Name = "Form1";
-            this.Text = "Upload File Google Drive";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSize = new System.Drawing.Size(379, 75);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.progressBar);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ProgressForm";
+            this.Text = "ProgressForm";
+            this.Load += new System.EventHandler(this.ProgressForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
-
-        private void labelNameAddress_Click(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void labelTextAcc_Click(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void labelTextFile_Click(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void labelTextLUd_Click(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button buttonSelectFile;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button buttonUpdate;
-        private System.Windows.Forms.ListBox listBoxFiles;
-        private System.Windows.Forms.Button buttonRemove;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label labelNameAddress;
-        private System.Windows.Forms.Button buttonChangeAccount;
-
+        #endregion
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label statusLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,200 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
-using System.Threading;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace UploadGoogleDrive
+namespace ProgressForm
 {
     public partial class Form1 : Form
     {
-        private OpenFileDialog openFileDialog;
-        private List<string> filePaths;
-        private GoogleDriveService googleDriveService;
-        private bool isChangingAccount;
-        private ProgressForm progressForm;
-
         public Form1()
         {
             InitializeComponent();
-            Control.CheckForIllegalCrossThreadCalls = false;
-
-            InitializeOpenFileDialog();
-            InitializeFields();
-            AttachEventHandlers();
         }
-
-        private void InitializeOpenFileDialog()
-        {
-            openFileDialog = new OpenFileDialog
-            {
-                Multiselect = true,
-                Filter = "All files (*.*)|*.*"
-            };
-        }
-
-        private void InitializeFields()
-        {
-            filePaths = new List<string>();
-            googleDriveService = new GoogleDriveService();
-            progressForm = new ProgressForm();
-            buttonRemove.Enabled = false;
-        }
-
-        private void AttachEventHandlers()
-        {
-            buttonSelectFile.Click += ButtonSelectFile_Click;
-            buttonUpdate.Click += ButtonUpload_Click;
-            buttonRemove.Click += ButtonRemove_Click;
-            buttonChangeAccount.Click += ButtonChangeAccount_Click;
-            listBoxFiles.SelectedIndexChanged += ListBoxFiles_SelectedIndexChanged;
-            listBoxFiles.SelectionMode = SelectionMode.MultiExtended;
-        }
-
-        private async void Form1_Load(object sender, EventArgs e)
-        {
-            await googleDriveService.InitializeAsync();
-            DisplayUserEmail();
-        }
-        private void enableButtonUpdate()
-        {
-            buttonUpdate.Enabled = listBoxFiles.Items.Count > 0;
-        }
-
-        private void listBoxFiles_DragEnter(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-            {
-                e.Effect = DragDropEffects.Copy;
-            }
-            else
-            {
-                e.Effect = DragDropEffects.None;
-            }
-        }
-
-        private void listBoxFiles_DragDrop(object sender, DragEventArgs e)
-        {
-            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-            if (files != null && files.Length > 0)
-            {
-                foreach (var file in files)
-                {
-                    filePaths.Add(file);
-                    listBoxFiles.Items.Add(Path.GetFileName(file));
-                }
-                enableButtonUpdate();
-            }
-        }
-
-        private async void ButtonSelectFile_Click(object sender, EventArgs e)
-        {
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                foreach (var fileName in openFileDialog.FileNames)
-                {
-                    filePaths.Add(fileName);
-                    listBoxFiles.Items.Add(Path.GetFileName(fileName));
-                }
-                UpdateButtonState();
-            }
-        }
-
-        private async void ButtonUpload_Click(object sender, EventArgs e)
-        {
-            if (!filePaths.Any())
-            {
-                MessageBox.Show("Please select a file first.");
-                return;
-            }
-
-            progressForm.Show();
-            progressForm.ProgressBar.Maximum = filePaths.Count;
-            progressForm.ProgressBar.Value = 0;
-
-            var uploadTasks = filePaths.Select(filePath => Task.Run(() =>
-            {
-                googleDriveService.UploadFile(filePath);
-                Invoke((Action)UpdateProgressBar);
-            }));
-
-            await Task.WhenAll(uploadTasks);
-
-            progressForm.StatusLabel.Text = "Hoàn tất. Tất cả các tệp đã được tải lên thành công.";
-            await Task.Delay(2000);
-            progressForm.Hide();
-
-            listBoxFiles.Items.Clear();
-            filePaths.Clear();
-        }
-
-        private void UpdateProgressBar()
-        {
-            progressForm.ProgressBar.Value = Math.Min(progressForm.ProgressBar.Value + 1, progressForm.ProgressBar.Maximum);
-        }
-
-        private void ButtonRemove_Click(object sender, EventArgs e)
-        {
-            var selectedFiles = listBoxFiles.SelectedIndices.Cast<int>().OrderByDescending(i => i).ToList();
-            foreach (int index in selectedFiles)
-            {
-                string fileName = listBoxFiles.Items[index].ToString();
-                filePaths.Remove(filePaths.First(f => Path.GetFileName(f) == fileName));
-                listBoxFiles.Items.RemoveAt(index);
-            }
-            UpdateButtonState();
-        }
-
-        private void ListBoxFiles_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            buttonRemove.Enabled = listBoxFiles.SelectedIndices.Count > 0;
-        }
-
-        private void UpdateButtonState()
-        {
-            buttonUpdate.Enabled = listBoxFiles.Items.Count > 0;
-        }
-
-        private async void ButtonChangeAccount_Click(object sender, EventArgs e)
-        {
-            if (isChangingAccount) return;
-
-            try
-            {
-                isChangingAccount = true;
-                string credPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), ".credentials/drive-dotnet-quickstart.json");
-
-                if (Directory.Exists(credPath))
-                {
-                    Directory.Delete(credPath, true);
-                }
-
-                await googleDriveService.InitializeAsync();
-                DisplayUserEmail();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("An error occurred while changing accounts: " + ex.Message);
-            }
-            finally
-            {
-                isChangingAccount = false;
-            }
-        }
-
-        private void DisplayUserEmail()
-        {
-            try
-            {
-                var email = googleDriveService.GetUserEmail();
-                labelNameAddress.Text = email;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("An error occurred while fetching user info: " + ex.Message);
-            }
-        }
+        public ProgressBar ProgressBar => this.progressBar;
+        public Label StatusLabel => this.statusLabel;
     }
 }
